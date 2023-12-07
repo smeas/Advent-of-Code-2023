68bb634f06d2c312b755e780f593fb8e622b3f84
Jonatan Johansson <smeas@users.noreply.github.com>
2023-12-07 08:32:04 +0100

    Day 07 (part 1)

---
+++ 07/solution.py
@@ -0,0 +1,60 @@
+#
+# Day 07
+# Solution 1: 248812215
+# Solution 2:
+#
+#
+
+#data = open("example.txt").read().splitlines()
+data = open("input.txt").read().splitlines()
+
+CARDS = "AKQJT98765432"
+
+def parse_line(s):
+	hand, bid = s.split()
+	return hand, int(bid)
+
+def get_type_order(hand):
+	a, b, c, d, e = sorted(hand)
+	if a==b==c==d==e:
+		return 0
+	if a==b==c==d or b==c==d==e:
+		return 1
+	if a==b and c==d==e or a==b==c and d==e:
+		return 2
+	if a==b==c or b==c==d or c==d==e:
+		return 3
+	if a==b and c==d or a==b and d==e or b==c and d==e:
+		return 4
+	if a==b or b==c or c==d or d==e:
+		return 5
+	return 6
+
+def get_second_orderings(hand):
+	return [*map(lambda x: CARDS.index(x), hand)]
+
+def solve_1():
+	solution = 0
+	hands = [*map(parse_line, data)]
+	#print([*map(lambda x: (x[0], get_type_order(x[0])), hands)])
+
+	ordered_hands = reversed(sorted(hands, key=lambda x: (get_type_order(x[0]), *get_second_orderings(x[0]))))
+	for i, hand in enumerate(ordered_hands):
+		#print(i, hand)
+		hand, bid = hand
+		#print(bid * (i + 1))
+		solution += bid * (i + 1)
+
+	return solution
+
+
+
+def solve_2():
+	return 0
+
+first_solution = solve_1()
+print(first_solution)
+assert first_solution == 248812215
+
+second_solution = solve_2()
+print(second_solution)
